838f268bb94ec7aba891ef54cd9af7134d2275fa
Jonathan Nielsen <jonathan@majj.dk>
2026-04-09 12:51:17 +0200

    minor improvement

--- shell_scripts/full_pipeline.sh
+++ shell_scripts/full_pipeline.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
+if [ $# -eq 0 ]
+  then
+    echo "No data path supplied, exiting!"
+    echo "Example usage: ./full_pipeline.sh /path/to/data_folder"
+    exit 1
+fi
 file="$1"
 source /home/airlab/ros2_ws/install/setup.bash
 ros2 run airlab_lidar_3dgs pose_estimator "$file"
